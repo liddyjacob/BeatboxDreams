90d4e1b441199fcb5d8baebc831c9adcae810054
liddyjacob <liddyjacob@gmail.com>
2019-09-28 19:49:22 -0400

    Added skeleton of drumtools lib

---
+++ drumtools.py
@@ -0,0 +1,15 @@
+"""drumtools.py
+A set of functions for setting up
+and maintaining the drumset class
+The drumset class is located inside of here.
+"""
+
+class drumset:
+
+    def __init__(self, name):
+
+        pass
+
+def setUpDrumset(name):
+""" Make a new folder for the drumset class to read """
+    pass
